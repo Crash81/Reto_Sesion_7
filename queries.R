ac4f33a7b6490881490aecbd4840252442a6adc0
Alexander <galex5908@gmail.com>
2021-01-28 02:09:49 +0000

    Reto 1

---
+++ queries.R
@@ -0,0 +1,19 @@
+install.packages("DBI")
+install.packages("RMySQL")
+install.packages("ggplot2")
+install.packages("dplyr")
+library("DBI")
+library("RMySQL")
+library("ggplot2")
+library("dplyr")
+
+BaseDatos <- dbConnect(drv = RMySQL::MySQL(),dbname = "shinydemo",host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",username = "guest",password = "guest")
+
+Languages <- dbGetQuery(BaseDatos, "select * from CountryLanguage")
+
+Percent_spanish <-  Languages %>% filter(Language == "Spanish")
+
+Percent_spanish_df <- as.data.frame(Percent_spanish)
+
+Percent_spanish_df %>% ggplot(aes( x = CountryCode, y=Percentage)) + 
+  geom_col()
